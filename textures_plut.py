118580a194d6a43b2d79fca6cb41513621dff4ca
Guilherme M. Miranda <alchemist.software@proton.me>
2024-09-27 19:06:59 -0300

    done with tnt shit

--- textures_plut.py
+++ textures_plut.py
@@ -19,32 +19,23 @@ all_maps = [
 ]
 
 sidedef_arr: list[tuple[str, str]] = [
-    ('SLADRIP1', 'T_SLAD01'),
-    ('SLADRIP3', 'T_SLAD01'),
-    ('SLAD2',    'T_SLAD01'),
-    ('SLAD3',    'T_SLAD01'),
-    ('SLAD4',    'T_SLAD01'),
-    ('SLAD5',    'T_SLAD01'),
-    ('SLAD6',    'T_SLAD01'),
-    ('SLAD7',    'T_SLAD01'),
-    ('SLAD8',    'T_SLAD01'),
-    ('SLAD9',    'T_SLAD01'),
-    ('SLAD10',   'T_SLAD01'),
-    ('SLAD11',   'T_SLAD01'),
-    ('BLODGR1',  'T_BLOD1'),
-    ('BLODGR2',  'T_BLOD1'),
-    ('BLODGR3',  'T_BLOD1'),
-    ('BLODGR4',  'T_BLOD1'),
-    ('BLOD3',    'T_BLOD1'),
-    ('WFALL1',   'TNTWALL1'),
-    ('WFALL2',   'TNTWALL1'),
-    ('WFALL3',   'TNTWALL1'),
-    ('WFALL4',   'TNTWALL1'),
-    ('FALL3',    'TNTWALL1'),
-    ('SW1GSTON', 'SW1TNT1'),
-    ('SW2GSTON', 'SW2TNT1'),
-    ('SW1SKULL', 'SW1TNT2'),
-    ('SW2SKULL', 'SW2TNT2'),
+    ('DBRAIN1', 'MC01'),
+    ('MC2', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('MC', 'MC01'),
+    ('DBRAIN4', 'MC01'),
 ]
 
 
@@ -56,20 +47,20 @@ def log(line: str) -> None:
     log_file.write(line + '\n')
 
 
-def sidedef_list(editor: MapEditor, init_tex: str, tex: str):
+def sidedef_list(editor: MapEditor, init_tex: str, desired_tex: str):
     for single_sidedef in editor.sidedefs:
         # Loop
         if single_sidedef.tx_up == init_tex:
-            log(f'Found hi tex {init_tex} replaced by {tex}')
+            log(f'Found hi tex {init_tex} replaced by {desired_tex}')
         # through
         if single_sidedef.tx_mid == init_tex:
-            log(f'Found mid tex {init_tex} replaced by {tex}')
+            log(f'Found mid tex {init_tex} replaced by {desired_tex}')
         # all
         if single_sidedef.tx_low == init_tex:
-            log(f'Found low tex {init_tex} replaced by {tex}')
+            log(f'Found low tex {init_tex} replaced by {desired_tex}')
 
 
-base = WAD(from_file='cdce-addon-tnt.wad')
+base = WAD(from_file='cdce-addon-plutonia.wad')
 
 
 for map_slot in all_maps:
--- textures_tnt.py
+++ textures_tnt.py
@@ -4,22 +4,43 @@ log_file = None
 log_path = 'tnt_textures.log'
 
 all_maps = [
+    'MAP01',
+    'MAP02',
+    'MAP03',
+    'MAP04',
+    'MAP05',
     'MAP06',
+    'MAP07',
     'MAP08',
+    'MAP09',
     'MAP10',
     'MAP11',
+    'MAP12',
     'MAP13',
+    'MAP14',
+    'MAP15',
     'MAP16',
+    'MAP17',
+    'MAP18',
     'MAP19',
     'MAP20',
     'MAP21',
+    'MAP22',
+    'MAP23',
+    'MAP24',
+    'MAP25',
+    'MAP26',
+    'MAP27',
+    'MAP28',
+    'MAP29',
     'MAP30',
     'MAP31',
-
+    'MAP32',
 ]
 
 sidedef_arr: list[tuple[str, str]] = [
     ('SLADRIP1', 'T_SLAD01'),
+    ('SLADRIP2', 'T_SLAD01'),
     ('SLADRIP3', 'T_SLAD01'),
     ('SLAD2',    'T_SLAD01'),
     ('SLAD3',    'T_SLAD01'),
@@ -36,15 +57,15 @@ sidedef_arr: list[tuple[str, str]] = [
     ('BLODGR3',  'T_BLOD1'),
     ('BLODGR4',  'T_BLOD1'),
     ('BLOD3',    'T_BLOD1'),
-    ('WFALL1',   'TNTWALL1'),
-    ('WFALL2',   'TNTWALL1'),
-    ('WFALL3',   'TNTWALL1'),
-    ('WFALL4',   'TNTWALL1'),
-    ('FALL3',    'TNTWALL1'),
-    ('SW1GSTON', 'SW1TNT1'),
-    ('SW2GSTON', 'SW2TNT1'),
-    ('SW1SKULL', 'SW1TNT2'),
-    ('SW2SKULL', 'SW2TNT2'),
+    ('WFALL1',   'T_WALL1'),
+    ('WFALL2',   'T_WALL1'),
+    ('WFALL3',   'T_WALL1'),
+    ('WFALL4',   'T_WALL1'),
+    ('FALL3',    'T_WALL1'),
+    ('SW1GSTON', 'SW1TNTA'),
+    ('SW2GSTON', 'SW2TNTA'),
+    ('SW1SKULL', 'SW1TNT1'),
+    ('SW2SKULL', 'SW2TNT1'),
 ]
 
 
@@ -57,16 +78,21 @@ def log(line: str) -> None:
 
 
 def sidedef_list(editor: MapEditor, init_tex: str, tex: str):
+    index = 0
     for single_sidedef in editor.sidedefs:
+        sector = single_sidedef.sector
+        off_x = single_sidedef.off_x
+        off_y = single_sidedef.off_y
         # Loop
         if single_sidedef.tx_up == init_tex:
-            log(f'Found hi tex {init_tex} replaced by {tex}')
+            log(f'  #{index}\tHi  {init_tex}\tx\t{off_x} y\t{off_y}\t-> {tex} := {sector}')
         # through
         if single_sidedef.tx_mid == init_tex:
-            log(f'Found mid tex {init_tex} replaced by {tex}')
+            log(f'  #{index}\tMid {init_tex}\tx\t{off_x} y\t{off_y}\t-> {tex} := {sector}')
         # all
         if single_sidedef.tx_low == init_tex:
-            log(f'Found low tex {init_tex} replaced by {tex}')
+            log(f'  #{index}\tLow {init_tex}\tx\t{off_x} y\t{off_y}\t-> {tex} := {sector}')
+        index += 1
 
 
 base = WAD(from_file='cdce-addon-tnt.wad')
@@ -79,4 +105,5 @@ for map_slot in all_maps:
         desired = sidedef[1]
         map_edit = MapEditor(base.maps[map_slot])
         sidedef_list(map_edit, initial, desired)
+    base.to_file('./doom-addon-tnt.wad')
 
